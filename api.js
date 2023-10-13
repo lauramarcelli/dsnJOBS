95459c1cf4edd09597ee3cc398715ec52dfdd6fa
lauramarcelli <laura.marcelli@gmail.com>
2023-10-13 12:17:49 -0300

    agregando searchbar y analizando funcion agregar trabajo

--- api.js
+++ api.js
@@ -21,6 +21,28 @@ getDsnJobs = async () =>{
 getDsnJobs ();
 
 // agregar un nuevo puesto de trabajo --- POST--addDsnJob()
+
+const addDsnJob = async () => {
+    try {
+        const response = await fetch('https://6524190dea560a22a4e96ab1.mockapi.io/api/jobs', {
+            method: 'POST',
+            body: JSON.stringify(showingNewJob(jobs)),
+            headers: {'Content-Type': 'application/json; charset=UTF-8',
+    },
+}
+);
+
+const data = await response.json();
+console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+        
+
+
+
 // editar -- PATCH para editar parcialmente -- PUT si queremos cambiarle todos los campos--editDsnJob(id)
 // borrar -- DELETE--deleteDsnJob(id)
 
