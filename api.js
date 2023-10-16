2b9f84214c3e76d1e52788d6b32a21e35e5e528f
lauramarcelli <laura.marcelli@gmail.com>
2023-10-16 18:28:31 -0300

    agregando funciones para agregar trabajo nuevo

--- api.js
+++ api.js
@@ -1,15 +1,10 @@
 const hide = (selector) => $(selector).classList.add("visually-hidden");
 const show = (selector) => $(selector).classList.remove("visually-hidden");
 const cleanContainer = (selector) => ($(selector).innerHTML = "");
-
-/*const showView = (view) =>{
-    $$(".view").forEach((view) => view.classList.add("visually-hidden"));
-    $(`#${view}`).classList.remove("visually-hidden")
-};*/
-
+ 
 
 getDsnJobs = async () =>{
-    show("#spinner")
+    spinnerEffect();
     let response = await fetch('https://6524190dea560a22a4e96ab1.mockapi.io/api/jobs');
     let data = await response.json();
     setTimeout(() => {
@@ -17,14 +12,25 @@ getDsnJobs = async () =>{
     }, 2000);
 };
 getDsnJobs ();
+//Filtros
+const filterCountry =(data) => {
+    const countries = []
+    data.forEach((element) => {
+        if(!countries.includes(element.location)){
+            countries.push(element.location)
+        }
+    });
+    console.log(countries);
+    return countries;
+}
 
 // agregar un nuevo puesto de trabajo --- POST--addDsnJob()
 
-const addDsnJob = async () => {
+const addDsnJob = async (job) => {
     try {
         const response = await fetch('https://6524190dea560a22a4e96ab1.mockapi.io/api/jobs', {
             method: 'POST',
-            body: JSON.stringify(showingNewJob(jobs)),
+            body: JSON.stringify(job),
             headers: {'Content-Type': 'application/json; charset=UTF-8',
     },
 }
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const initializeJob = (data) => {
+    showJobs(data);
+    filterCountry(data);
+}
+
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
@@ -8,18 +13,21 @@ const showJobs = (jobs) =>{
         let row = document.createElement("div")
         row.setAttribute("class", "row")
         row.classList.add("gap-3");
-        for ( let { name, description, id } of jobs){
+        for ( let { position, description, location, seniority, id } of jobs){
             row.innerHTML += `
             <div class="card col-2 d-grid gap-2 d-md-block">
                 <div class="card-body justify-content-center">
-                    <h4 class="card-title">${name}</h4>
+                    <h4 class="card-title">${position}</h4>
                     <p class="job-description">${description}</p>
+                    <div class="mb-3">
+                    <span class="badge text-bg-secondary">${location}</span>
+                    <span class="badge text-bg-secondary">${seniority}</span>
+                    </div>
                 </div>
                 <a href="#" class='btn btn-warning button p-1' onclick=getCharacterById(${id})>See Info+</a>
             </div>
         `;
         $("#jobs-all").appendChild(row);
-        hide("#spinner");
         }
 
         } else {
@@ -27,7 +35,39 @@ const showJobs = (jobs) =>{
         }
 };
 
+
+const addNewJob = () => {
+    let newJob = {
+        position : $("#position-form").value,
+        description : $("#description-form").value,
+        location : $("#location-form").value,
+        seniority : $("#seniority-form").value,
+        benefits : {
+            "vacation": $("#vacations-form").value,
+            "health_insurance": $("#health-form").value,
+            "internet_paid": $("#internet-form").value,
+        },
+
+        salary : $("#salary-form").value,
+        languages : [$("#languages-1").value,]
+
+    }
+
+    console.log(newJob);
+    addDsnJob(newJob);
+
+}
 //DOM EVENTOS//
+
+
+
+const spinnerEffect = () => {
+    show(["#spinner"]);
+
+    setTimeout(() => {
+        hide(["#spinner"]);
+    }, 2000)
+};
 
 $("#btn-newJob").addEventListener("click", () => {
     show("#newJob-section");
@@ -36,4 +76,15 @@ $("#btn-newJob").addEventListener("click", () => {
     hide("#searchbar");
     hide("#getJob-img")
 });
+
+$("#btn-add-NewJob").addEventListener("click", (e) => {
+    e.preventDefault();
+    addNewJob();
+    show("#searchbar")
+    show("#getJob-img")
+    show("#jobs-all");
+    hide("#newJob-section");
+  });
+
+window.addEventListener("load", initializeJob)
 
